cb10a43831454b612739c68181d4bd59769a44fb
Vince Nguyen <ntgv1993@gmail.com>
2021-09-01 10:00:32 -0500

    Finished HH

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -13,9 +13,26 @@ fizzbuzz(16);
 */
 
 const fizzbuzz = num => {  
-  
+    
+    let arr = [];
+
+    let i = 1;
+    
+    while(i <= num){
+        let str = '';
+
+        if(i % 5 === 0 && i % 3 === 0) str += 'fizzbuzz';
+        else if(i % 5 === 0) str += 'buzz'
+        else if(i % 3 === 0) str += 'fizz'
+            
+        arr.push(str || i);
+        i++;
+    }
+
+    return arr;
 };
 
+console.log(fizzbuzz(29)); 
 /*
 
 Extension: 
@@ -36,7 +53,35 @@ fizzbuzzbazz(22);
 */
 
 const fizzbuzzbazz = num => {
-  
-};
+    
+    let arr = [];
+    
+    let i = 1;
+
+    while(i <= num){
+        str = '';
+
+        if(i % 3 === 0) {
+            if(i % 5 === 0) {
+                if(i % 7 === 0) str += 'fizzbuzzbazz';
+                else str += 'fizzbuzz';
+            }
+            else if(i % 7 === 0) str += 'fizzbazz';
+            else str += 'fizz';
+        }
+        else if(i % 5 === 0) {
+            if(i % 7 === 0) str += 'buzzbazz'
+            else str += 'buzz';
+        }
+        else if(i % 7 === 0) str += 'bazz';
+        
+        arr.push(str || i);
+        i++;
+    }
+
+    return arr;
+};  
+
+console.log(fizzbuzzbazz(22));
 
 module.exports = {fizzbuzz, fizzbuzzbazz};
