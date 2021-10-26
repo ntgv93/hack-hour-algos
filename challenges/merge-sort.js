61fa2e7191096d04a5c52ef551368f7fdb0fdaa8
Vince Nguyen <ntgv1993@gmail.com>
2021-10-26 09:42:17 -0500

    Solved merge-sort algo

--- challenges/merge-sort.js
+++ challenges/merge-sort.js
@@ -12,8 +12,34 @@
 
 */
 
+// merge sorted arrays into a sorted array
+const merge = (arr1, arr2) => {
+  const sortedArr = [];
+
+  while(arr1.length && arr2.length){
+
+    // pick the smaller element among the smallest elements of left and right sub arrays
+    if(arr1[0] < arr2[0])
+      sortedArr.push(arr1.shift());
+    else
+      sortedArr.push(arr2.shift());
+  }
+
+  // Concatenate any left over elements in case we didn't go through entire sub arrays
+  return [...sortedArr, ...arr1, ...arr2];
+}
+
 const mergeSort = array => {
 
+  // if there is only 1 element left in the array, it is considered sorted and returned
+  if(array.length < 2)
+    return array;
+
+  const left = array.slice(0, array.length/2);
+  const right = array.slice(array.length/2, array.length);
+  
+  // recursively merge left and right sub arrays starting starting with smallest
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 module.exports = { mergeSort };
