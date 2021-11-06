01c01fec183e45cd16c863b454b9205846887d07
Vince Nguyen <ntgv1993@gmail.com>
2021-11-06 12:47:31 -0500

    Solved with kadane's algo

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -46,15 +46,36 @@ const maxSubarray = (arr) => {
   return max;
 }
 
-console.log(maxSubarray([15, 20, -5, 10]));
 /*
-  Extension: solve in linear time and constant space
-  Hint: Kadane's Algorithm
-  https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
+Extension: solve in linear time and constant space
+Hint: Kadane's Algorithm
+https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
 */
 
+// Dynamic programming - if a problem can be broken down to an accumulation of solutions of smaller subproblems, then store solutions of smaller subproblems to minimize computation time
+// e.g. array.reduce((a, c) => a + c); (to find the sum of the whole array, the sum at each index is an accumulation of itself and all previous sums. 
+// Therefore, to calculate sum with each iteration, one would not have to re-add all previous elements, instead only having to add the already calculated sum of all previous elemenets plus the element itself)
+// Kadane's algorithm - an example of dynamic programming
+// local maximum at index i is element at index i or the sum of element at index i + local-maximum from previous sub arrays, whichever one is larger
 const kadanesMaxSubarray = (arr) => {
+
+  if(!arr || !arr.length)
+    return 0;
   
+  let local_max = 0;
+  let global_max = -Infinity;
+  
+  for(let i = 0; i < arr.length; i++){
+    local_max = Math.max(arr[i], arr[i] + local_max);
+    
+    if(local_max > global_max){
+      global_max = local_max;
+    }
+  }
+  
+  return global_max;
 }
+
+console.log(kadanesMaxSubarray([15, 20, -5, -10]));
 
 module.exports = { maxSubarray, kadanesMaxSubarray };
