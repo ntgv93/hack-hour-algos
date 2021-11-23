02a09444bd02fec24b3c7ccbe6a6ae986742d91c
Vince Nguyen <ntgv1993@gmail.com>
2021-11-23 00:33:39 -0600

    Now finds different ways to seats can be arranged

--- challenges/seating-students.js
+++ challenges/seating-students.js
@@ -16,3 +16,72 @@ Output:4
 Input:8, 1, 8
 Output:6
 */
+
+/*
+  [][x]
+  [][]
+  [][x]
+  [x][]
+  [][]
+  [x][]
+*/
+
+function seatingStudents(arr) { 
+  if(!arr || !arr.length)
+    return "Invalid input argument";
+
+  const seats = arr[0];
+  const rows = seats / 2;
+
+  // create a grid to mimic the seats layout and set default value of each seat to false (not occupied)
+  const grid = [...new Array(rows)].map(() => [false, false]);
+
+  // mark the taken seats as true (occupied)
+  for(let i = 1; i < arr.length; i++){
+    // odd seats are on the left, even seats are on the right
+    if(arr[i] % 2 === 0)
+      grid[arr[i] / 2 - 1][1] = true;
+    else 
+      grid[Math.floor(arr[i] / 2)][0] = true;
+  }
+
+  // declare a counter to keep track of possible seatings
+  let seatings = 0;
+
+  // loop up to the 2nd to last row of the grid and check adjacent seats
+  // for each iteration, check current row and next row
+  for(let row = 0; row < rows - 1; row++){
+    // if there are 2 adjacent seats, increment counter
+    
+    // check right neighbor
+    if(!grid[row][0] && !grid[row][1])
+      seatings++;
+
+    // check bottom left neighbor
+    if(!grid[row][0] && !grid[row + 1][0])
+      seatings++;
+
+    // check top right vs bottom right neighbor
+    if(!grid[row][1] && !grid[row + 1][1])
+      seatings++;
+  }
+
+  // check bottom row
+  if(!grid[rows - 1][0] && !grid[rows - 1][1])
+    seatings++;
+
+  // return the number of seatings
+  return seatings; 
+}
+   
+// [6, 4]
+/*
+  [x, x]
+  [x, 4]
+  [x, x]
+*/
+// possible seats = 4;
+
+// keep this function call here 
+console.log(seatingStudents([12, 2, 6, 7, 11]));
+
