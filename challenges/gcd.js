4cdebee6f7afcdccc68622a2c554cf6a929a4aa6
Vince Nguyen <ntgv1993@gmail.com>
2021-12-17 18:31:10 -0600

    Added time and space complexity

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -4,13 +4,19 @@
 // find the remainder of a / b, with a being the larger number and b being the smaller number
 // if the remainder is 0, then return the previous divisor which is b
 // if not, then find the remainder of b / current remainder, and continue until remainder is 0
+
+// time complexity: O(log(a+b))
+  // time complexity of this algo is the number of steps it would require to reduce a to 0
+// space complexity: O(log(a+b)) - recursion
 const gcd = (a, b) => {
   if(a === 0)
     return b;
-
+  
   return gcd(b % a, a);
 }
 
+// time complexity: O(n * log(a+b));
+// space complexity: O(log(a+b)) - recursion
 const gcdArr = arr => {
   // iterate through array and find gcd of 2 adjacent numbers
   let result = arr[0];
